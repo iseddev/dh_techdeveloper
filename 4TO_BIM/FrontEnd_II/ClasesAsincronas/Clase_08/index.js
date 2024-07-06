756e0b0b5170901eddee6916b1558ee24f40b5a3
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-07-06 01:53:21 -0600

    Se agrega ejercicio de la clase asíncrona #8

---
+++ 4TO_BIM/FrontEnd_II/ClasesAsincronas/Clase_08/index.js
@@ -0,0 +1,6 @@
+// String de películas separadas por ;
+let bestMovies = "El Padrino; El Padrino II; El Padrino III; El Padrino IV"
+
+// Array de películas
+let bestMoviesArray = bestMovies.split(";")
+console.log(bestMoviesArray)
