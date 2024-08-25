8354ecc9bc1deb012c39121f9bc77c589283bdb4
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-08-25 15:22:32 -0600

    Se integran actualizaciones a la clase 14 tanto virtual como presencial

--- 5TO_BIM/Backend_I/Clases/C14_Spring_SpringBoot/Presencial/C14S_SpringBoot/src/main/java/com/example/C14S_SpringBoot/C14SSpringBootApplication.java
+++ 5TO_BIM/Backend_I/Clases/C14_Spring_SpringBoot/Presencial/C14S_SpringBoot/src/main/java/com/example/C14S_SpringBoot/C14SSpringBootApplication.java
@@ -3,12 +3,11 @@ package com.example.C14S_SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Annotation
 @SpringBootApplication
 public class C14SSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(C14SSpringBootApplication.class, args);
-
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(C14SSpringBootApplication.class, args);
+  }
 }
--- 5TO_BIM/Backend_I/Clases/C14_Spring_SpringBoot/Presencial/C14S_SpringBoot/src/main/java/com/example/C14S_SpringBoot/controller/HelloWorldController.java
+++ 5TO_BIM/Backend_I/Clases/C14_Spring_SpringBoot/Presencial/C14S_SpringBoot/src/main/java/com/example/C14S_SpringBoot/controller/HelloWorldController.java
@@ -4,17 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // Annotations => En este caso vamos a simular una APIRest
+
 @RequestMapping("/dh_spring")
+
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return("<h1>Hellow World!!!</h1>");
-    }
+  // HTTP Methods => GET, POST, PUT, DELETE, etc. Para este caso vamos a usar @GetMapping
+  @GetMapping("/hello")
+  public String helloWorld() { return("<h1>Petición GET realizada => Hellow World!!!</h1>"); }
 
-    @GetMapping("/goodbye")
-    public String goodbyeWorld() {
-        return("<h1>Goodbye World!!!</h1>");
-    }
+  @GetMapping("/goodbye")
+  public String goodbyeWorld() { return("<h1>Petición GET realizada => Goodbye World!!!</h1>"); }
 }
