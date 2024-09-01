0f270dcf71cdec1d59295bef5bda44cbdc75ae19
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-09-01 14:46:17 -0600

    Se actualizan archivos de la clase 13

--- 5TO_BIM/Backend_I/Clases/C13_MAVEN/Presencial/C13S_MAVEN_MesaTrabajo/src/main/java/odontologo/dao/BD_Odontologo.java
+++ 5TO_BIM/Backend_I/Clases/C13_MAVEN/Presencial/C13S_MAVEN_MesaTrabajo/src/main/java/odontologo/dao/BD_Odontologo.java
@@ -35,6 +35,6 @@ public class BD_Odontologo {
 
     public static Connection getConnection() throws Exception {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/Databases/examenbckn", "root", "");
+        return DriverManager.getConnection("jdbc:h2:./examenbckn", "root", "");
     }
 }
