0a8dd6b46932000aa2ccf671c30984480dad5214
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-12-07 15:25:02 -0600

    Mesa de trabajo Clase 13 - V2

--- 6TO_BIM/Frontend_III/Clase_13/c13_mesa_trabajo/src/App.jsx
+++ 6TO_BIM/Frontend_III/Clase_13/c13_mesa_trabajo/src/App.jsx
@@ -1,36 +1,18 @@
-import { useState } from "react";
-
-import LanguageContext from "./context/LanguageContext";
-import { languages } from "./languages/languages";
+import { LanguageProvider } from "./context/LanguageContext";
 
 import Body from "./components/Body";
 import Navbar from "./components/NavBar";
 
 import "./App.css";
 function App() {
-	const [language, setLanguage] = useState(languages.english);
-
-	const handleChangeLanguage = () => {
-		setLanguage(() => {
-			//TIP: Function that exchanges one language for another (by clicking the button)
-			if (language.id === "EN") {
-				return languages.portuguese;
-			} else if (language.id === "PTBR") {
-				return languages.spanish;
-			} else {
-				return languages.english;
-			}
-		});
-	};
-
 	return (
 		<div className="App">
 			<>
 				{/* TIP: Add provider of LanguageContext */}
-				<LanguageContext.Provider value={{ language, handleChangeLanguage }}>
+				<LanguageProvider>
 					<Navbar />
 					<Body />
-				</LanguageContext.Provider>
+				</LanguageProvider>
 			</>
 		</div>
 	);
--- 6TO_BIM/Frontend_III/Clase_13/c13_mesa_trabajo/src/context/LanguageContext.jsx
+++ 6TO_BIM/Frontend_III/Clase_13/c13_mesa_trabajo/src/context/LanguageContext.jsx
@@ -1,8 +1,31 @@
-import { createContext } from "react";
+import { useState, createContext, useCallback } from "react";
 
 import { languages } from "../languages/languages";
 
 // TIP: Use createContext and start English as the default language
 const LanguageContext = createContext(languages.english);
+
+export const LanguageProvider = ({ children }) => {
+	const [language, setLanguage] = useState(languages.english);
+
+	const handleChangeLanguage = useCallback(() => {
+		setLanguage(() => {
+			if (language.id === "EN") {
+				return languages.portuguese;
+			} else if (language.id === "PTBR") {
+				return languages.spanish;
+			} else {
+				return languages.english;
+			}
+		});
+	}, [language]);
+
+	// TIP: Use the provider to pass the language and the function to change the language
+	return (
+		<LanguageContext.Provider value={{ language, handleChangeLanguage }}>
+			{children}
+		</LanguageContext.Provider>
+	);
+};
 
 export default LanguageContext;
