7d435feb05e7b37433f0e998500cb8d120e973b5
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-12-07 17:11:10 -0600

    Integración de footer y sus estilos

--- 6TO_BIM/Frontend_III/proyecto_final/src/components/footer/Footer.jsx
+++ 6TO_BIM/Frontend_III/proyecto_final/src/components/footer/Footer.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className={styles.footer}>
+			<p className={styles.footerItem}>Proyecto Final Frontend III</p>
+			<p className={styles.footerItem}>DIC-2024</p>
+			<Link className={styles.footerItem} to="https://github.com/iseddev" target="_blank">
+				iseddev
+			</Link>
+		</footer>
+	);
 };
 
 export default Footer;
