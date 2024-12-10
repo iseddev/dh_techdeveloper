b90592bfdc3583d8e0c7299f5633aaf40c0e681e
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2024-12-10 00:18:41 -0600

    Integración de un contador de personajes favoritos en el menú de navegación

--- 6TO_BIM/Frontend_III/proyecto_final/src/components/headerNav/HeaderNav.jsx
+++ 6TO_BIM/Frontend_III/proyecto_final/src/components/headerNav/HeaderNav.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import LINKS from "../../data/MenuLinks";
 
 import styles from "./HeaderNav.module.css";
+import { useContext } from "react";
+import FavoritesContext from "../../context/FavoritesContext";
 
 const HeaderNav = () => {
+	const { favorites } = useContext(FavoritesContext);
 	return (
 		<div className={styles.navContainer}>
 			<nav className={styles.headerNav}>
@@ -13,8 +16,16 @@ const HeaderNav = () => {
 				</Link>
 				<ul className={styles.headerNav__list}>
 					{LINKS.map((link, index) => (
-						<li key={`${index}-${link.page}`}>
-							<Link to={link.to}>{link.page}</Link>
+						<li className={styles.headerNav__item} key={`${index}-${link.page}`}>
+							{link.page.toLowerCase() === "favorites" && favorites.length > 0 ? (
+								<>
+									<Link to={link.to}>{link.page}</Link>
+									<span className={styles.navFavoritesCounter}>{favorites.length}</span>
+								</>
+							) : (
+								<Link to={link.to}>{link.page}</Link>
+							)}
+							{/* <Link to={link.to}>{link.page}</Link> */}
 						</li>
 					))}
 				</ul>
